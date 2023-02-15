8c4487b033151c39c5a85c5ae46c00086bdd8fb6
Kevin Bahner <kbahner@kent.edu>
2023-02-14 22:30:57 -0500

    dded main page and changes a bunch of stuff

---
+++ src/routes/Home/+layout.svelte
@@ -0,0 +1,72 @@
+<script>
+import { signout } from "../stores/firebaseConfig"
+import { onAuthStateChanged } from 'firebase/auth';
+import {auth} from '../stores/firebaseConfig'
+let currentUser
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user
+  } 
+});
+
+
+</script>
+{#if currentUser}
+<nav>
+    <img id="title" alt="">
+    <div class="navbar">
+    <a href="/">Active Bets</a>
+    <a href="/">Calendar</a>
+    <a href="/">Leaderboard</a>
+    <a href="/">Kent Game Schedule</a>
+    </div>
+    <button class="Signout" on:click={signout}>Signout</button>
+
+</nav>
+
+<slot>
+
+</slot>
+{:else}
+<h1>You got signed out</h1>
+<a href="/" style="color: black;">sign back in here</a>
+{/if}
+
+<style>
+        
+    #title {
+    height: 90px;
+    width: 150px;
+    background-image: url(../images/spinning-fish.gif);
+    background-size: cover;
+    background-position: center;
+}
+
+
+.Signout {
+    float: right;
+    display: flex;
+    align-items: center;
+    
+}
+nav {
+    position: sticky;
+    top: 0px;
+    display: flex;
+    padding: 0.5%;
+    background-color: #324a69;
+}
+.navbar {
+  float: left;
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+a {
+    color: white;
+    text-decoration: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.2em;
+    margin-left: 6%;
+}
+</style>
---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+import { onAuthStateChanged } from 'firebase/auth';
+import {auth} from '../stores/firebaseConfig'
+let currentUser
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user
+
+  } 
+});
+</script>
+
+{#if currentUser}
+<section class="background">
+<div>
+    <h1 style="color: white; margin:0;">Hi There {currentUser.email}</h1>
+</div>
+</section>
+{:else}
+<p></p>
+{/if}
+
+
+
+<style>
+    .background {
+        background-color: #000f24;
+        height: 100vh;
+    }
+
+</style>
