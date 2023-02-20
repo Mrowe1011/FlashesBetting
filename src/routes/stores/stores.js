2248e2e68001cc636c2481f536a1c73c4abd3dc9
Kevin Bahner <kbahner@kent.edu>
2023-02-20 13:51:53 -0500

    add stores

---
+++ src/routes/stores/stores.js
@@ -0,0 +1,37 @@
+import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	signInWithEmailAndPassword,
+	setPersistence,
+	browserSessionPersistence,
+	signOut
+} from 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+let currentUser;
+
+export function onSubmit(username, password) {
+	signInWithEmailAndPassword(auth, username, password)
+		.catch((error) => {
+			alert('Invalid Login: ' + error);
+		})
+		.then(() => {
+			setPersistence(auth, browserSessionPersistence);
+			currentUser = auth.currentUser;
+			location.href = '/homePage'; // Svelte normally appends .html to location hrefs but doesn't for this because its a js file. Remove the .html while your developing otherwise it doesn't work
+		});
+}
+
+export function signout() {
+	signOut(auth)
+		.then(() => {
+			currentUser = auth.currentUser;
+			location.href = '/';
+		})
+		.catch((error) => {
+			alert(error);
+		});
+}
