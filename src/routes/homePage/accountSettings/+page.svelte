86834a1b1799d37fc31fe8780e58e8bb1aab9956
Benjamin Goodhart <bengoodhart16@gmail.com>
2023-03-14 22:24:49 -0400

    Added a section for new style
    New style on account settings page. Still WIP

--- src/routes/homePage/accountSettings/+page.svelte
+++ src/routes/homePage/accountSettings/+page.svelte
@@ -3,6 +3,14 @@
 	import { getDoc, doc, updateDoc } from 'firebase/firestore';
 	import { deleteUser, updateEmail } from 'firebase/auth';
 
+
+	function confirmDelete() {
+  // display the dialog box
+  	const dialogBox = document.getElementById('delete-dialog');
+  	dialogBox.classList.remove('hidden');
+}
+
+	
 	let Fname;
 	async function submitChangeUsername() {
 		await updateDoc(doc(db, 'users', $currentUser.email), {
@@ -29,6 +37,7 @@
 		profile = querySnapshot.data();
 		return profile;
 	}
+	
 </script>
 
 <div class="background">
@@ -65,6 +74,67 @@
 			</div>
 		</div>
 	{/await}
+	{#await getData() then profile}
+
+	<div class="container mx-auto my-8">
+		<form class="max-w-lg mx-auto">
+		  <h1 class="text-3xl font-bold mb-6">Settings</h1>
+	  
+		  <!-- Example settings -->
+		  <div class="mb-6">
+			<label class="block font-bold mb-2" for="username">Change {profile.name}'s Username</label>
+			<div class="flex items-center">
+				<form on:submit|preventDefault={submitChangeEmail}>
+					<input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="New username" bind:value={Fname}>
+					<button class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+						Change
+					</button>
+			  </form>
+			</div>
+		  </div>
+	  
+		  <div class="mb-6">
+			<label class="block font-bold mb-2" for="email">Change Email</label>
+			<div class="flex items-center">
+			  <input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="New email">
+			  <button class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+				Change
+			  </button>
+			</div>
+		  </div>
+	  
+		  <div class="mb-6">
+			<label class="block font-bold mb-2" for="current-email">Current Email:</label>
+			<div class="bg-gray-100 rounded-lg py-2 px-3">
+			  <p class="text-gray-800">{$currentUser.email}</p>
+			</div>
+		  </div>
+	  
+		  <div class="mb-6 flex justify-end">
+			<button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" id="deleteAccountButton">
+			  Delete Account
+			</button>
+		  </div>
+		</form>
+		
+		<!-- Confirmation dialog box -->
+		<div class="fixed inset-0 bg-gray-700 bg-opacity-75 flex items-center justify-center" id="deleteAccountModal" style="display: none">
+		  <div class="bg-white rounded-lg shadow-lg p-6">
+			<p class="text-lg font-semibold mb-4">Are you sure you want to delete your account?</p>
+			<div class="flex justify-end">
+			  <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline" type="button" id="cancelDeleteButton">
+				Cancel
+			  </button>
+			  <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" id="confirmDeleteButton">
+				Delete
+			  </button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  
+	  {/await}
+
 </div>
 
 <style>
@@ -75,6 +145,7 @@
 
 	.container {
 		display: flex;
+		text-align: center;
 		flex-direction: column;
 		width: 60%;
 		background-color: rgba(255, 255, 255, 0.945);
